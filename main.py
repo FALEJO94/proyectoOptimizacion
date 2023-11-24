0a90aa32c6da736b6e55c3aab9f778a01ce486cd
Esteban Zuluaga <esteban.zuluaga@konexinnovation.com.co>
2023-11-23 19:06:26 -0500

    inital project structure

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# st.set_page_config(layout="wide")
+
+st.markdown("<h1 style='text-align: center;'>Métodos Numéricos y Optimización 🤘💥</h1>", unsafe_allow_html=True)
+st.markdown("<h3>Calculo de raices</h3><hr style='margin: 5px 0px;'>", unsafe_allow_html=True)
